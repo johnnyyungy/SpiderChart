5a29eda4091fa01dce12703e2216e6321a2f37b2
John Yung <johnnyyungy@gmail.com>
2025-03-14 13:30:05 +0000

    Initial Commit

---
+++ RadarChart/RadarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadarChartApp.swift
+//  RadarChart
+//
+//  Created by Yung Bros on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct RadarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
