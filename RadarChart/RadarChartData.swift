e37e523bd1e6f1670134866e96e78857d00e8363
John Yung <johnnyyungy@gmail.com>
2025-03-14 13:42:47 +0000

    first commit

---
+++ RadarChart/RadarChartData.swift
@@ -0,0 +1,8 @@
+//
+//  RadarChartData.swift
+//  RadarChart
+//
+//  Created by Yung Bros on 14/03/2025.
+//
+
+import Foundation
---
+++ RadarChart/RadarChartView.swift
@@ -0,0 +1,18 @@
+//
+//  RadarChartView.swift
+//  RadarChart
+//
+//  Created by Yung Bros on 14/03/2025.
+//
+
+import SwiftUI
+
+struct RadarChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RadarChartView()
+}
